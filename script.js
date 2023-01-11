e2fc0a1c11accc583661f301b751520d575b3a1b
Maksym Kolontaievskyi <maksym.kolontaievskyi@ukr.net>
2023-01-11 19:19:12 +0200

    Check which key was pressed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.body.addEventListener('keydown', function(ev) {
-  console.log(ev.key);
+  if (ev.key === 'ArrowUp') {
+    console.log('Up');
+  } else if (ev.key === 'ArrowDown') {
+    console.log('Down');
+  } else if (ev.key === 'ArrowLeft') {
+    console.log('Left');
+  } else if (ev.key === 'ArrowRight') {
+    console.log('Right');
+  }
 });
