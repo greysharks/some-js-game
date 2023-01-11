3d05cea0786ddd9e0ff4d60147e73a1f02b4a4b1
Maksym Kolontaievskyi <maksym.kolontaievskyi@ukr.net>
2023-01-11 22:10:24 +0200

    Create class named Player and add moving up and down functionality (test)

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.body.addEventListener('keydown', function(ev) {
+class Player {
+  constructor() {
+    this.reference = document.getElementById('player');
+  }
+  MoveDown() {
+    this.reference.style.marginTop = '20px';
+  }
+  MoveUp() {
+    this.reference.style.marginTop = '0px';
+  }
+}
+
+const player = new Player();
+
+document.body.addEventListener('keydown', function (ev) {
   if (ev.key === 'ArrowUp') {
+    player.MoveUp();
     console.log('Up');
   } else if (ev.key === 'ArrowDown') {
+    player.MoveDown();
     console.log('Down');
   } else if (ev.key === 'ArrowLeft') {
     console.log('Left');
