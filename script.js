6572d1fd8de6ffa8c2e3192f18d8cf51ef9abc64
Maksym Kolontaievskyi <maksym.kolontaievskyi@ukr.net>
2023-01-11 22:34:54 +0200

    Add x in y coordinates in class Player

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 class Player {
   constructor() {
     this.reference = document.getElementById('player');
+    this.y = 0;
+    this.x = 0;
   }
   MoveDown() {
     this.reference.style.marginTop = '20px';
+    this.y = Number.parseInt(this.reference.style.marginTop);
   }
   MoveUp() {
     this.reference.style.marginTop = '0px';
+    this.y = Number.parseInt(this.reference.style.marginTop);
   }
   MoveRight() {
     this.reference.style.marginLeft = '20px';
+    this.x = Number.parseInt(this.reference.style.marginLeft);
   }
   MoveLeft() {
     this.reference.style.marginLeft = '0px';
+    this.x = Number.parseInt(this.reference.style.marginLeft);
   }
 }
 
@@ -21,15 +27,15 @@ const player = new Player();
 document.body.addEventListener('keydown', function (ev) {
   if (ev.key === 'ArrowUp') {
     player.MoveUp();
-    console.log('Up');
+    console.log('y =', player.y);
   } else if (ev.key === 'ArrowDown') {
     player.MoveDown();
-    console.log('Down');
+    console.log('y =', player.y);
   } else if (ev.key === 'ArrowLeft') {
     player.MoveLeft();
-    console.log('Left');
+    console.log('x =', player.x);
   } else if (ev.key === 'ArrowRight') {
     player.MoveRight();
-    console.log('Right');
+    console.log('x =', player.x);
   }
 });
